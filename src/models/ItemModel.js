04ec6a8f56a14e08b6ed9a760a89626ebdccfb0b
objectum <objectum.ru@gmail.com>
2021-09-29 22:12:42 +0300

    todo

--- src/models/ItemModel.js
+++ src/models/ItemModel.js
@@ -1,7 +1,8 @@
 /* eslint-disable no-whitespace-before-property */
 /* eslint-disable eqeqeq */
 
-import {Record} from "objectum-client";
+import objectumClient from "objectum-client";
+const Record = objectumClient.Record;
 
 export default class ItemModel extends Record {
 	toJSON () {
